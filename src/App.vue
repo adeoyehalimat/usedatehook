6e09d55cb327956f7f06c4e11f2dfaf3c5b4a2bd
Adeoye Halimat <adeoyehalimah@gmail.com>
2023-12-13 01:24:23 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <use-date/>
+  <div>
+    <div class="container">
+      {{ title }}
+      <br />
+    </div>
+    Date: {{ updatedDate }}
+    <br />
+    Day: {{ getDate() }}
+    <br />
+    Month: {{ getMonth() }}
+    <br />
+    <div>
+      Add Day:
+      <input type="number" v-model.number="daysAddition" style="margin-bottom: 2px;" />
+      <br />
+      Add Month:
+      <input type="number" v-model.number="monthsAddition" />
+      <br />
+      <button @click="handleChangeDate">
+        Change The World
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import useDate  from './components/useDateMain.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {useDate},
+  data() {
+    return {
+      title: 'Present date:',
+      date: new Date(),
+      daysAddition: 0,
+      monthsAddition: 0,
+    };
+  },
+  computed: {
+    updatedDate() {
+      return this.handleChangedDate().toString();
+    },
+  },
+  methods: {
+    getDate() {
+      return this.date.getDate();
+    },
+    getMonth() {
+      return this.date.getMonth() + 1;
+    },
+    addDay(numberOfDays, mainDate = this.date) {
+      const newDate = new Date(mainDate);
+      newDate.setDate(newDate.getDate() + numberOfDays);
+      return newDate;
+    },
+    addMonth(numberOfMonths, mainDate = this.date) {
+      const newDate = new Date(mainDate);
+      newDate.setMonth(newDate.getMonth() + numberOfMonths);
+      return newDate;
+    },
+    handleChange({ target }) {
+      const { name, value } = target;
+      if (name === 'day') {
+        this.daysAddition = parseInt(value);
+      }
+      if (name === 'month') {
+        this.monthsAddition = parseInt(value);
+      }
+    },
+    handleChangedDate() {
+      const newDate = this.addMonth(this.monthsAddition, this.addDay(this.daysAddition, this.date));
+      return newDate;
+    },
+    handleChangeDate() {
+      this.title = 'Calculated date:';
+      this.date = this.handleChangedDate();
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+input,
+button {
+  margin: 2px;
+  border-radius: 4px;
+  padding: 2px;
+}
+.container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 20px;
+  font-weight: 100;
 }
 </style>
---
+++ src/components/useDateMain.vue
@@ -0,0 +1,32 @@
+<!-- useDateComposable.vue -->
+<script>
+import { ref } from 'vue';
+
+export default function useDate() {
+  let date = ref(new Date());
+
+  let getDay = () => {
+    return date.value.getDay() + 1;
+  };
+
+  let getMonth = () => {
+    return date.value.getMonth() + 1;
+  };
+
+  const addDay = (numberOfDays, mainDate = date.value) => {
+    const newDate = new Date(mainDate);
+    newDate.setDate(newDate.getDate() + numberOfDays);
+    return newDate;
+    // N.B if day after adding is greater than the number of days for that month, date returned should be a new month date
+  };
+
+  const addMonth = (numberOfMonths, mainDate = date.value) => {
+    const newDate = new Date(mainDate);
+    newDate.setMonth(newDate.getMonth() + numberOfMonths);
+    return newDate;
+    // N.B if month after adding is greater than 11, date returned should be a new year
+  };
+
+  return { date, getDay, getMonth, addDay, addMonth };
+}
+</script>
